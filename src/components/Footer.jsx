b46a8055222f5156a0340b2f4d7413186912f306
amaurygarcia <amaurygp9@gmail.com>
2024-10-06 15:28:30 -0500

    fix footer word

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
         <div className="mx-auto max-w-screen-xl px-4 pt-44 sm:px-6 lg:px-8">
             <div className="mx-auto max-w-md">
             <strong className="block text-center text-xl font-bold text-gray-900 sm:text-3xl">
-                ¿Quieres conctatarme?
+                ¿Quieres contactarme?
             </strong>
 
             <form className="mt-6">
