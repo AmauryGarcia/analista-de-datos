1418a98fcce27a18fd4ab6da9ff2e744d6147913
Amaury Garcia <122466867+AmauryGarcia@users.noreply.github.com>
2024-10-08 16:29:03 -0500

    agg target to cv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,6 +25,7 @@ export default function Hero() {
               </a>
               <a
                 href="/CV-Amaury-Garcia.pdf"
+                target="_blank"
                 title="CV"
                 className="rounded-md bg-sky-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-sky-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
               >
